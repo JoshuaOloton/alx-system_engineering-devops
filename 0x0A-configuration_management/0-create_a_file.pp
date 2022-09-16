f49d1c6a8fcecb9c6c17799bbb04ac3813f550a5
Joshua Oloton <olotonjoshua@gmail.com>
2022-09-16 13:11:12 +0100

    added task 1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,9 @@
+# create a file in /tmp.
+
 file { '/tmp/school':
-    owner    => 'www-data',
-    group    => 'www-data',
-    mode     => '0744',
-    contents => 'I love Puppet'
+    ensure  => 'present',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
 }
