a0da8593556168005a63c684fc1f86e845cfdcd3
Joshua Oloton <olotonjoshua@gmail.com>
2022-09-27 18:56:53 +0100

    fixed requirement eror

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,7 @@
 # automate the task of creating a custom HTTP header response
-
 exec { 'command':
   command  => 'apt-get -y update;
-  apt -y install nginx;
+  apt-get -y install nginx;
   sudo sed -i "s/server_name _;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
   service nginx restart',
   provider => shell,
