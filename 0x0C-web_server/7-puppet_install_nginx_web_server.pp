4bbbd9a3065e9702df09dcf8d674a99a83a22d44
Joshua Oloton <olotonjoshua@gmail.com>
2022-09-27 18:07:12 +0100

    renamed 404 page

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -22,7 +22,7 @@ file { '/var/www/html/index.html':
   content => "Hello World!\n",
 }
 
-file { '/var/www/html/404.html':
+file { '/var/www/html/error-page.html':
   content => "Ceci n'est pas une page\n",
 }
 
@@ -33,17 +33,20 @@ file { 'Nginx default config file':
 "server {
         listen 80 default_server;
         listen [::]:80 default_server;
-               root /var/www/html;
+        root /var/www/html;
+
         # Add index.php to the list if you are using PHP
         index index.html index.htm index.nginx-debian.html;
+
         server_name _;
+        
         location / {
                 # First attempt to serve request as file, then
                 # as directory, then fall back to displaying a 404.
                 try_files \$uri \$uri/ =404;
         }
-        error_page 404 /404.html;
-        location  /404.html {
+        error_page 404 /error-page.html;
+        location  /error-page.html {
             internal;
         }
         
