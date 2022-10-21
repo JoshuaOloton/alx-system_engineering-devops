9511d719d53f38b7cb705a35240cb7c0dc198ed6
Joshua Oloton <olotonjoshua@gmail.com>
2022-10-21 12:36:49 +0100

    fixed format issues

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,11 +8,10 @@ if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com'
     users = requests.get('{}/users/{}'.format(url, argv[1])).json()
     userId = users.get('id')
-    username = users.get('name')
+    username = users.get('username')
     todos = requests.get(
         '{}/todos'.format(url), params={'userId': userId}).json()
 
-    empName = users.get("name")
     with open('{}.csv'.format(argv[1]), mode='w', newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
 
