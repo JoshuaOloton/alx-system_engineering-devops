bb96afa991382583afc3cd8d49422a4a8bed26fa
Joshua Oloton <olotonjoshua@gmail.com>
2022-10-21 12:13:57 +0100

    export csc

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,16 +3,18 @@
 import requests
 from sys import argv
 
-url = 'https://jsonplaceholder.typicode.com'
-users = requests.get('{}/users/{}'.format(url, argv[1])).json()
-userId = users.get('id')
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com'
+    users = requests.get('{}/users/{}'.format(url, argv[1])).json()
+    userId = users.get('id')
 
-todos = requests.get('{}/todos'.format(url)).json()
-todos = list(filter(lambda x: x.get('userId') == userId, todos))
-comp_todos = list(filter(lambda x: x.get('completed'), todos))
+    todos = requests.get(
+        '{}/todos'.format(url),
+        params={'userId': userId}).json()
 
-empName = users.get("name")
-print("Employee {} is done with ({}/{})".format(
-    empName, len(comp_todos), len(todos)))
-for todo in comp_todos:
-    print("\t {}".format(todo.get('title')))
+    completed = list(filter(lambda x: x.get('completed'), todos))
+
+    empName = users.get("name")
+    print("Employee {} is done with ({}/{})".format(
+        empName, len(completed), len(todos)))
+    [print("\t {}".format(todo.get('title'))) for todo in completed]
