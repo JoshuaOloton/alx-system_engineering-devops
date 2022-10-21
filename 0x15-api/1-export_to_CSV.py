b7d8c75ca6d1c0e168b54a0da3e063e909dc6824
Joshua Oloton <olotonjoshua@gmail.com>
2022-10-21 12:20:40 +0100

    fixed requirement issue

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """Returns to-do list information for a given employee ID."""
+import csv
 import requests
 from sys import argv
 
-url = 'https://jsonplaceholder.typicode.com'
-users = requests.get('{}/users/{}'.format(url, argv[1])).json()
-userId = users.get('id')
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com'
+    users = requests.get('{}/users/{}'.format(url, argv[1])).json()
+    userId = users.get('id')
+    username = users.get('name')
+    todos = requests.get(
+        '{}/todos'.format(url), params={'userId': userId}).json()
 
-todos = requests.get('{}/todos'.format(url)).json()
-todos = list(filter(lambda x: x.get('userId') == userId, todos))
-comp_todos = list(filter(lambda x: x.get('completed'), todos))
+    empName = users.get("name")
+    with open('{}.csv'.format(argv[1]), mode='w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
 
-empName = users.get("name")
-print("Employee {} is done with ({}/{})".format(
-    empName, len(comp_todos), len(todos)))
-for todo in comp_todos:
-    print("\t {}".format(todo.get('title')))
-
-
+        for todo in todos:
+            writer.writerow(
+                [userId, username, todo.get("completed"), todo.get("title")])
